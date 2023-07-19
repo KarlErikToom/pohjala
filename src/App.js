23b98011021bac72d5ec7007a2274bac2891c49f
Karl <toom.karl42@gmail.com>
2023-07-19 23:06:23 +0300

    added forest series page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import Landing from "./pages/Landing";
 import Specials from "./pages/Specials";
 import Cellar from "./pages/Cellar";
+import Forest from "./pages/Forest";
 function App() {
   return (
     <Router>
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" element={<Landing />} />
           <Route path="/specials" element={<Specials />} />
           <Route path="/cellar" element={<Cellar/>} />
+          <Route path="/forest" element={<Forest />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Forest.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Logo from "../assets/pohjala-logo (1).png";
+
+function Forest() {
+    const [beers, setBeers] = useState([]);
+    async function getData() {
+      const { data } = await axios.get("https://api.punkapi.com/v2/beers?page=4");
+      setBeers(data);
+    }
+    useEffect(() => {
+      getData();
+    }, []);
+  return (
+    <div>
+
+    <section id="forest">
+        <figure className="specials__logo">
+          <a href="">
+            {" "}
+            <img className="specials__logo--img" src={Logo} alt="" />{" "}
+          </a>
+        </figure>
+      <div className="forest__text">
+        <h1>
+          The forest has a deep influence on Estonian culture and cuisine. This
+          heritage is embodied in our Forest Series. <br /> <br /> In these editions, we twist
+          rare botanicals, forest ingredients and Estonian folk-medicine with
+          ancient methods into absolutely extraordinary beers.
+        </h1>
+      </div>
+    </section>
+     <section id="special__beer">
+     <div className="container">
+       <div className="row">
+         <div className="beers__wrapper">
+           {beers.slice(0, 25).map((beer, index) => (
+               <div className="beer">
+               <figure className="beer__figure">
+                 <img
+                   src={beers.length > 0 && beer.image_url}
+                   alt=""
+                   className="beer__img"
+                   />
+               </figure>
+
+               <div className="beer__info">
+                 <h2 className="beer__name">{beer?.name}</h2>
+                 <h2 className="beer__abv">{beer?.abv}%</h2>
+                 <h3 className="beer__tagline">{beer?.tagline}</h3>
+                     {index % 4 === 3 &&(<span className="discontinued">discontinued </span>)}
+                     {index % 6 === 4 &&(<span className="discontinued">sold out </span>)}
+                 <p className="beer__description">
+                   Taste: {beer?.description}
+                 </p>
+                 <details>
+                   <summary>Read more...</summary>
+
+                   <p className="beer__malts">
+                     Malts:{" "}
+                     {beer?.ingredients.malt
+                       .map((malt) => malt.name)
+                       .join(", ")}
+                   </p>
+                   <p className="beer__hops">
+                     Hops:{" "}
+                     {beer?.ingredients.hops
+                       .map((hops) => hops.name)
+                       .join(", ")}
+                   </p>
+                   <p className="beer__brewed">
+                     First brewed: {beer?.first_brewed}
+                   </p>
+                 </details>
+               </div>
+             </div>
+           ))}
+         </div>
+       </div>
+     </div>
+   </section>
+           </div>
+  );
+}
+
+export default Forest;
